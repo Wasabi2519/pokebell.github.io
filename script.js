a0cec4440544d6955bb452b581f0c24cd96511d0
たしなむわさび | Wasabi2519 <fuga.maeji@gmail.com>
2023-05-17 14:56:04 +0900

    一応完成

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
-  function convertToCharacter() {
-    let input = document.getElementById("inputNumber").value;
-    
+function convertToHiragana() {
+    const input = document.getElementById("inputBox").value;
+    const hiraganaMap = {
+      "01": "あ",
+      "02": "い"
+    };
+
     let result = "";
     for (let i = 0; i < input.length; i += 2) {
-      let code = input.substr(i, 2);
-      let character = String.fromCharCode(parseInt(code));
-      result += character;
+      const pair = input.slice(i, i + 2);
+      const converted = hiraganaMap[pair];
+      result += converted ? converted : "";
     }
-    if (input == 11) {
-        alert("あ");
-      }
-      if (input == 12) {
-          alert("い");
-        }
-      if (input == 13) {
-          alert("う");
-        }
-      if (input == 14) {
-          alert("え");
-        }
-      if (input == 15) {
-          alert("お");
-        }
-      if (input == 16) {
-          alert("A");
-        }
-      if (input == 17) {
-          alert("B");
-        } 
-      if (input == 18) {
-          alert("C");
-        }
-      if (input == 19) {
-          alert("D");
-        }
-    
-    alert(result);
+
+    document.getElementById("output").textContent = result;
   }
-  
-  
